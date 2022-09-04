87ea9e8f60bd3008660ab1402ec5fcaa406ed4be
mohdsameerid <mohdsameeredu@gmail.com>
2022-09-04 10:39:56 +0530

    chaining by super keyword

--- JS/Inheritance/script.js
+++ JS/Inheritance/script.js
@@ -1,12 +1,34 @@
-class employee{
-    constructor(name){
-        console.log("Employee class "+name);
+// class employee{
+//     constructor(name){
+//         console.log("Employee class "+name);
+//     }
+// }
+// class manager extends employee{
+//     constructor(name){
+//         super("John");
+//         console.log("Manager class "+name);
+//     }
+// }
+// let e1 = new manager("Samir");
+
+
+// chaining by super Keyword ( C --> B --> A ) 
+class A{
+    constructor(){
+        console.log("Class A");
     }
 }
-class manager extends employee{
+class B extends A{
     constructor(name){
-        super("John");
-        console.log("Manager class "+name);
+        super();
+        console.log("Class B with "+name);
     }
 }
-let e1 = new manager("Samir");
+class C extends B{
+    constructor(){
+        super("Samir");
+        console.log("Class C");
+    }
+}
+
+let obj = new C();
