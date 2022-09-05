ba6d70fe150b64446c3e94f9ba0e29ea7034e817
mohdsameerid <mohdsameeredu@gmail.com>
2022-09-05 20:43:53 +0530

    aliases iplemented

--- JS/Modules/script.js
+++ JS/Modules/script.js
@@ -1,10 +1,10 @@
-import { str, sayHi, user } from "./user.js";
+import { str, sayHi as hi, user } from "./user.js";
 
 document.body.innerHTML =  str;  // display on HTML page
 console.log(str); // displaying on console
 
-document.body.innerHTML = sayHi("samir");
-console.log(sayHi("I am an indian"))
+document.body.innerHTML = hi("samir");
+console.log(hi("I am an indian"))
 
 
 let obj  = new user(); // class user object created
