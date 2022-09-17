a0e0865fdcaebf66366c739746cb52e6ce92295f
mohdsameerid <mohdsameeredu@gmail.com>
2022-09-17 23:26:06 +0530

    question 1

---
+++ Output/script.js
@@ -0,0 +1,11 @@
+// (1) 
+function sayHi() {
+    console.log(name);
+    console.log(age);
+    var name = "Rasel";
+    let age = 21;
+  }
+  sayHi();
+  // ans -> undefined , ReferenceError
+//  ------------------------------------------------------------ 
+
