bd412ba826bc3187d583fef0fde478294691a19b
mohdsameerid <mohdsameeredu@gmail.com>
2022-09-21 23:59:10 +0530

    counter using addEventListener ....

--- DemoJs/script.js
+++ DemoJs/script.js
@@ -2,40 +2,46 @@
 let count = document.querySelector(".count");
 count.innerHTML = 0;
 
-
-// //  Counter using addEventListener
-// let incrementbtn = document.querySelector(".incre");
-// incrementbtn.addEventListener("click", () => { // addEventListener
-//     var countnum = document.querySelector(".count").innerHTML;
-//     countnum++;
-//     count.innerHTML = countnum;
-// });
-// let dcrementBtn = document.querySelector(".dcre");
-// dcrementBtn.addEventListener("click", () => {
-//     countnum = document.querySelector(".count").innerHTML;
-//     countnum--;
-//     count.innerHTML = countnum;
-// });
+var countnum;
+//  Counter using addEventListener
+let incrementbtn = document.querySelector(".incre");
+incrementbtn.addEventListener("click", () => { // addEventListener
+    countnum = document.querySelector(".count").innerHTML;
+    countnum++;
+    count.innerHTML = countnum;
+});
+let dcrementBtn = document.querySelector(".dcre");
+dcrementBtn.addEventListener("click", () => {
+    if(document.querySelector(".count").innerHTML > 0){
+       countnum = document.querySelector(".count").innerHTML;
+       countnum--;
+       count.innerHTML = countnum;
+    }
+});
+let resetBtn = document.querySelector(".reset");
+resetBtn.addEventListener("click", () => {
+    document.querySelector(".count").innerHTML = 0;
+});
 
 
 
 // Counter using Normal Function 
-var countnum;
-function Increment(){
-        countnum = document.querySelector(".count").innerHTML;
-        countnum++;
-        count.innerHTML = countnum;
-}
-function Dcrement(){
-    if(countnum > 0){
-        countnum = document.querySelector(".count").innerHTML;
-        countnum--;
-        count.innerHTML = countnum;
-    }
-}
-function Reset(){
-    countnum = document.querySelector(".count").innerHTML;
-    countnum = 0;
-    count.innerHTML = countnum;
-    // console.log(countnum);
-}
+// var countnum;
+// function Increment(){
+//         countnum = document.querySelector(".count").innerHTML;
+//         countnum++;
+//         count.innerHTML = countnum;
+// }
+// function Dcrement(){
+//     if(countnum > 0){
+//         countnum = document.querySelector(".count").innerHTML;
+//         countnum--;
+//         count.innerHTML = countnum;
+//     }
+// }
+// function Reset(){
+//     countnum = document.querySelector(".count").innerHTML;
+//     countnum = 0;
+//     count.innerHTML = countnum;
+//     // console.log(countnum);
+// }
