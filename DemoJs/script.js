a7a6b903c3e5201eb2818860ef117fb678c8863f
mohdsameerid <mohdsameeredu@gmail.com>
2022-09-27 23:30:42 +0530

    counter code

--- DemoJs/script.js
+++ DemoJs/script.js
@@ -1,27 +1,27 @@
 
-let count = document.querySelector(".count");
-count.innerHTML = 0;
+// let count = document.querySelector(".count");
+// count.innerHTML = 0;
 
-var countnum;
-//  Counter using addEventListener
-let incrementbtn = document.querySelector(".incre");
-incrementbtn.addEventListener("click", () => { // addEventListener
-    countnum = document.querySelector(".count").innerHTML;
-    countnum++;
-    count.innerHTML = countnum;
-});
-let dcrementBtn = document.querySelector(".dcre");
-dcrementBtn.addEventListener("click", () => {
-    if(document.querySelector(".count").innerHTML > 0){
-       countnum = document.querySelector(".count").innerHTML;
-       countnum--;
-       count.innerHTML = countnum;
-    }
-});
-let resetBtn = document.querySelector(".reset");
-resetBtn.addEventListener("click", () => {
-    document.querySelector(".count").innerHTML = 0;
-});
+// var countnum;
+// //  Counter using addEventListener
+// let incrementbtn = document.querySelector(".incre");
+// incrementbtn.addEventListener("click", () => { // addEventListener
+//     countnum = document.querySelector(".count").innerHTML;
+//     countnum++;
+//     count.innerHTML = countnum;
+// });
+// let dcrementBtn = document.querySelector(".dcre");
+// dcrementBtn.addEventListener("click", () => {
+//     if(document.querySelector(".count").innerHTML > 0){
+//        countnum = document.querySelector(".count").innerHTML;
+//        countnum--;
+//        count.innerHTML = countnum;
+//     }
+// });
+// let resetBtn = document.querySelector(".reset");
+// resetBtn.addEventListener("click", () => {
+//     document.querySelector(".count").innerHTML = 0;
+// });
 
 
 
@@ -45,3 +45,19 @@ resetBtn.addEventListener("click", () => {
 //     count.innerHTML = countnum;
 //     // console.log(countnum);
 // }
+
+let count = document.querySelector(".count");
+count.innerHTML = 0;
+let val;
+function Increment(){
+    val = document.querySelector(".count").innerHTML;
+    val++;
+    count.innerHTML = val;
+}
+function Dcrement(){
+    val = document.querySelector(".count").innerHTML;
+    if(val > 0){
+        val--;
+        count.innerHTML = val;
+    }
+}
