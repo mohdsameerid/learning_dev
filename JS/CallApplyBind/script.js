c071a6f4a509831776c3443fab9dbd5c9dcea1cb
mohdsameerid <mohdsameeredu@gmail.com>
2022-10-04 11:37:34 +0530

    Apply()

--- JS/CallApplyBind/script.js
+++ JS/CallApplyBind/script.js
@@ -4,12 +4,12 @@ let person = {
     name: "Samir",
     age: 22,
     course: "Bca",
-    printDetails: function (){
+    printDetails: function (city, country, gender){
         console.log(this);
-        console.log(this.name);
+        console.log("And other details: "+city+" "+country+" "+gender);
     }
 };
-person.printDetails();
+// person.printDetails();
 
 let person2 = {
     name: "Hassen",
@@ -18,10 +18,21 @@ let person2 = {
 };
 
 // function borrowing OR call method 
-person.printDetails.call(person2);
+// person.printDetails.call(person2);
 
 /// output 
 //  {name: 'Samir', age: 22, course: 'Bca', printDetails: ƒ}
 //  Samir
 //  {name: 'Hassen', age: 22, course: 'Mtech'}
 //  Hassen
+
+
+// apply (apply me array hi pas kr dete hai)
+person.printDetails.call(person2,"kanpur","India","Male");
+person.printDetails.apply(person2,["Lucknow","India","Male"]);
+
+// output -> 
+// {name: 'Hassen', age: 22, course: 'Mtech'}
+//  And other details: kanpur India Male
+// {name: 'Hassen', age: 22, course: 'Mtech'}
+//  And other details: Lucknow India Male
