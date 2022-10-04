7dac26f14eb76c834d06e495fedcb6bf460c3ff5
mohdsameerid <mohdsameeredu@gmail.com>
2022-10-04 22:05:28 +0530

    ques 2

--- Output/script.js
+++ Output/script.js
@@ -1,11 +1,22 @@
 // (1) 
-function sayHi() {
-    console.log(name);
-    console.log(age);
-    var name = "Rasel";
-    let age = 21;
-  }
-  sayHi();
-  // ans -> undefined , ReferenceError
+// function sayHi() {
+//     console.log(name);
+//     console.log(age);
+//     var name = "Rasel";
+//     let age = 21;
+//   }
+//   sayHi();
+//   // ans -> undefined , ReferenceError
 //  ------------------------------------------------------------ 
+
+//ques-
+const object = {
+  message: 'Hello, World!',
+  logMessage() {
+      console.log(this.message); 
+      // What is logged? 
+  }
+};
+setTimeout(object.logMessage, 1000); 
+// undefined
 
