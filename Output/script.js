487640d61e054466ced78b5e4e9dbb0f0b406e66
mohdsameerid <mohdsameeredu@gmail.com>
2022-10-04 22:15:55 +0530

    ques 3 and 4

--- Output/script.js
+++ Output/script.js
@@ -9,14 +9,42 @@
 //   // ans -> undefined , ReferenceError
 //  ------------------------------------------------------------ 
 
-//ques-
-const object = {
-  message: 'Hello, World!',
-  logMessage() {
-      console.log(this.message); 
-      // What is logged? 
+//ques(1)-
+// const object = {
+//   message: 'Hello, World!',
+//   logMessage() {
+//       console.log(this.message); 
+//       // What is logged? 
+//   }
+// };
+// setTimeout(object.logMessage, 1000); 
+// output ->  undefined
+
+
+// Q3) How can you call logMessagefunction so that it logs "Hello, World!" ? 
+// const object1 = {
+//   message: 'Hello, World!'
+// };
+// function logMessage() {
+//   console.log(this.message);
+//   // "Hello, World!" 
+// } 
+// logMessage.call(object1);
+/// this is the way you call -> logMessage.call(object1);
+
+
+// Q4)
+const object3 = {
+  who: 'World',
+  greet() {
+      return `Hello, ${this.who}!`;
+  },
+  farewell: () => {
+      return `Goodbye, ${this.who}!`;
   }
 };
-setTimeout(object.logMessage, 1000); 
-// undefined
-
+console.log(object3.greet()); // What is logged? 
+console.log(object3.farewell()); // What is logged? 
+// outout -> 
+// Hello, World!
+// Goodbye, undefined!
