9692f8172bd2796bc795b2b3ea05f497736edf12
mohdsameerid <mohdsameeredu@gmail.com>
2022-10-07 15:56:56 +0530

    added greeting with color according to time

--- React/reactyout/src/index.js
+++ React/reactyout/src/index.js
@@ -5,24 +5,48 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
+
 let name = "Samir";
 let img1 = "https://picsum.photos/200/300";
 let img2 = "https://picsum.photos/202/300";
 let img3 = "https://picsum.photos/230/300";
+let h = {
+  fontFamily: "'Alkalami', serif"
+}
 
+////////////////////////////////////////////////////////////////////////////////////
+let currDate = new Date();
+currDate = currDate.getHours();
+let greeting  = "";
+let cssStyle = { };
+
+if(currDate >= 1 && currDate < 12){
+  greeting = "Good Morning";
+  cssStyle.color = "blue";
+}else if(currDate >= 12 && currDate < 19){
+  greeting = "Good AfterNoon";
+  cssStyle.color = "orange";
+}else{
+  greeting = "Good Night";
+  cssStyle.color = "black";
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-   <div className='box'>
-    <h1 className='h'>Hello My name is {name}</h1>
+  // <>
+  //  <div className='box'>
+  //   <h1 style={ {fontFamily: "'Alkalami', serif"} } >Hello My name is {name}</h1>
 
-    <div className='images'>
-      <img className='img' src={img1} />
-      <img className='img' src={img2} />
-      <img className='img' src={img3} />
-    </div>
-   </div>
+  //   <div className='images'>
+  //     <img className='img' src={img1} />
+  //     <img className='img' src={img2} />
+  //     <img className='img' src={img3} />
+  //   </div>
+  //  </div>
+  // </>
+
+  <>
+    <h1>Hello, <span style={cssStyle}> {greeting}</span></h1>
   </>
 );
 
