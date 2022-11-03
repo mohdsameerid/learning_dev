75238f3adc950028520970a1159b46f3a1f7a2b7
mohdsameerid <mohdsameeredu@gmail.com>
2022-11-03 23:48:17 +0530

    sum of two pair

--- Dsawithjs/sumOfTwoPair.js
+++ Dsawithjs/sumOfTwoPair.js
@@ -5,12 +5,10 @@
 function sumOfTwoPair(arr){
     for(let numbers of arr){
         for(let j = 1; j < arr.length; j++){
-            
             if(numbers + arr[j] == 0){
                 return [numbers,arr[j]];
             }
         }
-       
     }
 }
 console.log(sumOfTwoPair([-5,-4,-3,-2,0,2,4,6,8]));
