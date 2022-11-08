ec80563b3467ba185a17bd126d524dfc4d83e3e1
mohdsameerid <mohdsameeredu@gmail.com>
2022-11-08 22:39:00 +0530

    some changes

--- Dsawithjs/countUnique.js
+++ Dsawithjs/countUnique.js
@@ -6,23 +6,23 @@
 // (1) Approach for Count unique Numbers ..
 //    1 -> Make a Set of counter[items] using  Array
 //    2 -> Push that key to newArray / get last key that is your answer.
-// function abc(Array){
-//     let counter = {};
-//     let newArray = [];
+function abc(Array){
+    let counter = {};
+    let newArray = [];
 
-//     for(let items of Array){ // Make a Set of counter[items] using  Array
-//         counter[items] = (counter[items] || 0) + 1;
-//     }
-//     let newval;
-//     for(let key in counter){ 
-//         // newArray.push(key);  //  Push that key to newArray
-//         newval = key;         // last key get vahi answer ho jayega.
-//         // console.log(key)
-//     }
-//     return newval;
-// }
-// let res = abc([1,1,2,3,4,4,5,6,7,7,8]);
-// console.log(res);
+    for(let items of Array){ // Make a Set of counter[items] using  Array
+        counter[items] = (counter[items] || 0) + 1;
+    }
+    let newval;
+    for(let key in counter){ 
+        // newArray.push(key);  //  Push that key to newArray
+        newval = key;         // last key get vahi answer ho jayega.
+        // console.log(key)
+    }
+    return newval;
+}
+let res2 = abc([1,1,2,3,4,4,5,6,7,7,8]);
+console.log(res2);
 
 
 
