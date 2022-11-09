25a46ff3a358cc801444e757193dbbc00e9da73d
mohdsameerid <mohdsameeredu@gmail.com>
2022-11-10 00:01:42 +0530

    Remvome Duplicacy from Array

--- Dsawithjs/duplicateElem.js
+++ Dsawithjs/duplicateElem.js
@@ -18,3 +18,20 @@ function duplicateElement(Array){
 
 let res  = duplicateElement([28,302,303,202,4,2,302,2,4]);
 console.log(res);
+
+
+
+///////////////////////////////Remvome Duplicacy from Array//////////////////////////////
+let numbe = [1,2,4,2,1,3,1,2,1,3,342]
+let counters = {};
+let newArray = [];
+for(let items of numbers){
+    counters[items] = (counters[items] || 0) + 1;
+}
+let i = 0;
+for(let elem in counters){
+    newArray[i] = elem;
+    i++;
+}
+console.log("Orignal Numbers  : "+numbers);
+console.log("Revome duplicacy : "+newArray);
