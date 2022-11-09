e05701e21f176405eae7e4e5a1bba4c218255417
mohdsameerid <mohdsameeredu@gmail.com>
2022-11-09 21:38:58 +0530

    Reverse String

--- JS_Interview/script.js
+++ JS_Interview/script.js
@@ -487,3 +487,13 @@ function trimLeftSpace() {
 
 var result = trimLeftSpace("   Angry Bird   ");
 console.log(result);
+
+
+//////////////////////////////////Reverse String /////////////////////////////////////////////////
+
+let str = "w3villa";
+let newString = "";
+for(let i = str.length-1; i >= 0; i--){
+    newString += str[i];
+}
+console.log(newString);
