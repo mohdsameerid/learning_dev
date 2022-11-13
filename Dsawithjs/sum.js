d05f39a2eb56e01a1b11901245798ef8cf3b26a5
mohdsameerid <mohdsameeredu@gmail.com>
2022-11-13 22:14:36 +0530

    Sum of elements

---
+++ Dsawithjs/sum.js
@@ -0,0 +1,12 @@
+ // [1,2,3,4] -> input 
+ // 10 -> output 
+
+ function arr(Array){
+    let sum = 0;
+    for(let i = 0; i < Array.length; i++){
+        sum += Array[i];
+    }
+    return sum;
+ }
+ let res = arr([1,2,3,4]);
+ console.log(res); // 10
