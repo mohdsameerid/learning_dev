b1e8fd80650310397e1013bd1072f537caf91617
mohdsameerid <mohdsameeredu@gmail.com>
2022-11-14 22:29:58 +0530

    Sum of five Numbers & find out which number is greatest

--- Dsawithjs/sum.js
+++ Dsawithjs/sum.js
@@ -14,3 +14,27 @@
 
 
 
+// problem -> Sum of five Numbers & find out which number is greatest
+// 1,39,2,37,29 --> input
+// sum is 108 & greatest Number is 39
+
+function sumAndGreatest(n1,n2,n3,n4,n5){
+    let sum = n1+n2+n3+n4+n5;
+    // let assume n1 is greatest
+    let max = n1;
+    if(n2 > max){
+        max = n2;
+    }
+    if(n3 > max){
+        max = n3; 
+    }
+    if(n4 > max){
+        max = n4;
+    }
+    if(n5 > max){
+        max = n5;
+    }
+    console.log("Sum is: "+sum);
+    console.log("Greatest Number is: "+max);
+}
+sumAndGreatest(1,39,2,37,29);
