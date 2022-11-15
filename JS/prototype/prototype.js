3cda7689b1fc50a3ead2d7c1ea18e924cfcdf984
mohdsameerid <mohdsameeredu@gmail.com>
2022-11-15 13:01:52 +0530

    add sometthing in prototype .js

--- JS/prototype/prototype.js
+++ JS/prototype/prototype.js
@@ -28,25 +28,40 @@
 
 
 //////////////////////////////////Prototype Inheritance /////////////////////////////
-const obj3 = {
-    name: "Samir",
-    rollno: 200,
-    getRollNo: function(){
-        return this.rollno
-    },
-    getName: function(){
-        return this.name
-    }
-}
-// console.log(obj)
-const obj4 = {
-    rollno: "20",
-    // name : "Amit",
-    __proto__:obj3
-}
-const obj5 = {
-    class : "MCA",
-    __proto__:obj4
-}
-console.log(obj5.getRollNo()) // 20
-console.log(obj5.getName()) //  Samir
+// const obj3 = {
+//     name: "Samir",
+//     rollno: 200,
+//     getRollNo: function(){
+//         return this.rollno
+//     },
+//     getName: function(){
+//         return this.name
+//     }
+// }
+// // console.log(obj)
+// const obj4 = {
+//     rollno: "20",
+//     // name : "Amit",
+//     __proto__:obj3
+// }
+// const obj5 = {
+//     class : "MCA",
+//     __proto__:obj4
+// }
+// console.log(obj5.getRollNo()) // 20
+// console.log(obj5.getName()) //  Samir
+
+
+
+//////////////////////////////////////////////////////////////////////////////////
+const arr = ["grapes"];
+// console.log(arr);
+
+let array = new Array();
+console.log(array);
+let obj = new Object();
+console.log(obj)
+const func = new Function();
+console.log(func)
+
+
