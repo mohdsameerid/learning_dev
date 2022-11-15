75366bc06090e06daa3439f856af252c7940dd46
mohdsameerid <mohdsameeredu@gmail.com>
2022-11-15 12:32:18 +0530

    prototype ineritance

--- JS/prototype/prototype.js
+++ JS/prototype/prototype.js
@@ -20,4 +20,28 @@ const obj2 = {
     rollno: "20",
     __proto__:obj
 }
-console.log(obj2) // Samir
+console.log(obj2.name) // Samir
+
+
+
+
+
+//////////////////////////////////Prototype Inheritance /////////////////////////////
+const obj3 = {
+    name: "Samir",
+    rollno: 200,
+    getRollNo: function(){
+        return this.rollno
+    },
+    getName: function(){
+        return this.name
+    }
+}
+// console.log(obj)
+const obj4 = {
+    rollno: "20",
+    // name : "Amit",
+    __proto__:obj3
+}
+console.log(obj4.getRollNo()) // 20
+console.log(obj4.getName()) //  Samir
