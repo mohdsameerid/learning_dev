e8a96a96b29adb8e59c13f6c5a9de298c9c7b395
mohdsameerid <mohdsameeredu@gmail.com>
2022-11-16 23:19:32 +0530

    Separation of String to vowels and consonents

---
+++ Dsawithjs/seperation.js
@@ -0,0 +1,21 @@
+// String = "Muhammad Samir"   (input) 
+// consonents -> MhmmdSmr, vowels -> uaaai  (ouput)
+
+
+function seperater(String){
+    let consotents = "";
+    let vowels = "";
+    for(let items of String){
+        // console.log(items)
+        if((items == "a") || (items == "e") || (items == "i") || ( items == "o") || (items =="u")){
+            vowels += items;
+            // console.log(vowels)
+        }
+        else{
+            consotents += items;
+        }
+    }
+    console.log("Consonets: "+consotents);
+    console.log("Vowels: "+vowels);
+}
+seperater("Muhammad Samir");
