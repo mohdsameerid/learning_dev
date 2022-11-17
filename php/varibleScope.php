fe3edc74dad82e5ac19df8b070f7b372c00c03e9
mohdsameerid <mohdsameeredu@gmail.com>
2022-11-17 11:42:36 +0530

    PHP The static Keyword

--- php/varibleScope.php
+++ php/varibleScope.php
@@ -58,3 +58,30 @@ function myTest() {
 myTest();
 echo $y; // outputs 15
 ?>
+
+
+
+<!-- PHP The static Keyword -->
+<!-- Normally, when a function is completed/executed, all of its variables are deleted.
+ However, sometimes we want a local variable NOT to be deleted. We need it for a further 
+ job. To do this, use the static keyword when you first declare the variable: -->
+ <!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function myTest() {
+  static $x = 0;
+  echo $x;
+  $x++;
+}
+
+myTest();
+echo "<br>";
+myTest();
+echo "<br>";
+myTest();
+?> 
+
+</body>
+</html>
