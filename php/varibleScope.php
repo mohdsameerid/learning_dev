5f073fbb3fdf89d2b7d9fa04c7b988f534536101
mohdsameerid <mohdsameeredu@gmail.com>
2022-11-17 11:44:02 +0530

    PHP The static Keyword

--- php/varibleScope.php
+++ php/varibleScope.php
@@ -85,3 +85,9 @@ myTest();
 
 </body>
 </html>
+
+<!-- Then, each time the function is called, that variable will still have the information 
+it contained from the last time the function was called.
+
+Note: The variable is still local to the function. -->
+
