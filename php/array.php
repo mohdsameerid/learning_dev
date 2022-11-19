38c2ca6d6c09def4300b3ab7e240b43f353f9289
mohdsameerid <mohdsameeredu@gmail.com>
2022-11-19 23:09:42 +0530

    PHP Array

--- php/array.php
+++ php/array.php
@@ -1,5 +1,17 @@
-In PHP, there are three types of arrays:
+<!-- Array -->
+<!-- In PHP, there are three types of arrays: -->
 
-Indexed arrays - Arrays with a numeric index
+<!-- Indexed arrays - Arrays with a numeric index
 Associative arrays - Arrays with named keys
-Multidimensional arrays - Arrays containing one or more arrays
+Multidimensional arrays - Arrays containing one or more  -->
+
+
+<!-- Get The Length of an Array - The count() Function
+The count() function is used to return the length (the number of elements) of an array: -->
+
+<!-- Example -->
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+echo count($cars);
+?>
+// 3
