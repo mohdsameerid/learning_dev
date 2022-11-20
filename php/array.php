6f5cb5b9c0e1e2b933cc5e7efbc9f2e9787de5e2
mohdsameerid <mohdsameeredu@gmail.com>
2022-11-20 21:58:06 +0530

    PHP Assosicate  Array

--- php/array.php
+++ php/array.php
@@ -80,3 +80,15 @@ echo "Peter is " . $age['Peter'] . " years old.";
 ?>
 
 
+<!-- Loop Through an Associative Array
+To loop through and print all the values of an associative array, you could use a foreach loop, like this: -->
+
+<!-- Example -->
+<?php
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+foreach($age as $x => $x_value) {
+  echo "Key=" . $x . ", Value=" . $x_value;
+  echo "<br>";
+}
+?>
