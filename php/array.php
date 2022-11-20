9b8673596eaaee71cb5a3b49513f88c0d320a037
mohdsameerid <mohdsameeredu@gmail.com>
2022-11-20 21:55:17 +0530

    PHP indexed Array

--- php/array.php
+++ php/array.php
@@ -15,3 +15,27 @@ $cars = array("Volvo", "BMW", "Toyota");
 echo count($cars);
 ?>
 // 3
+
+
+
+
+
+<!-- PHP Indexed Arrays
+There are two ways to create indexed arrays:
+
+The index can be assigned automatically (index always starts at 0), like this: -->
+
+$cars = array("Volvo", "BMW", "Toyota");
+or the index can be assigned manually:
+
+$cars[0] = "Volvo";
+$cars[1] = "BMW";
+$cars[2] = "Toyota";
+
+<!-- The following example creates an indexed array named $cars, assigns three elements to it, and then prints a text containing the array values: -->
+
+<!-- Example -->
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+?>
