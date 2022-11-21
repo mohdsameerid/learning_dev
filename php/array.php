55b046bc3bbcfe1ee61286a62c6d0b94a2e48105
mohdsameerid <mohdsameeredu@gmail.com>
2022-11-21 21:30:20 +0530

    PHP 2D  Array

--- php/array.php
+++ php/array.php
@@ -92,3 +92,37 @@ foreach($age as $x => $x_value) {
   echo "<br>";
 }
 ?>
+
+
+
+<!-- PHP - Multidimensional Arrays -->
+<!-- A multidimensional array is an array containing one or more arrays. -->
+
+<!-- take a look at the following table: -->
+
+Name	    Stock	Sold
+Volvo	    22	    18
+BMW	        15	    13
+Saab	    5	    2
+Land Rover	17	    15
+We can store the data from the table above in a two-dimensional array, like this:
+
+$cars = array (
+  array("Volvo",22,18),
+  array("BMW",15,13),
+  array("Saab",5,2),
+  array("Land Rover",17,15)
+);
+
+<!-- Now the two-dimensional $cars array contains four arrays, 
+and it has two indices: row and column.
+
+To get access to the elements of the $cars array we must 
+point to the two indices (row and column): -->
+
+<?php
+echo $cars[0][0].": In stock: ".$cars[0][1].", sold: ".$cars[0][2].".<br>";
+echo $cars[1][0].": In stock: ".$cars[1][1].", sold: ".$cars[1][2].".<br>";
+echo $cars[2][0].": In stock: ".$cars[2][1].", sold: ".$cars[2][2].".<br>";
+echo $cars[3][0].": In stock: ".$cars[3][1].", sold: ".$cars[3][2].".<br>";
+?>
