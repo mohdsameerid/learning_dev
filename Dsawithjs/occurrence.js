ee5499a5c31018287013dd7b5f2d7a59a4b316ea
mohdsameerid <mohdsameeredu@gmail.com>
2022-11-23 22:53:05 +0530

    count occurrences in array

---
+++ Dsawithjs/occurrence.js
@@ -0,0 +1,21 @@
+// Javascript program to count occurrences
+// of an element
+ 
+ 
+    // Returns number of times x occurs in arr[0..n-1]
+    function countOccurrences(arr,n,x)
+    {
+        let res = 0;
+        for (let i=0; i<n; i++)
+        {
+            if (x == arr[i])
+                res++;
+        }
+        return res;
+    }
+     
+    arr=[1, 2, 2, 2, 2, 3, 4, 7 ,8 ,8]
+    let  n = arr.length;
+    let x = 2;
+    let res = countOccurrences(arr, n, x);
+    console.log(res)
