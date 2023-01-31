8b36441709dd9ca89d2915bd0239988b3c18ebd3
mohdsameerid <mohdsameeredu@gmail.com>
2023-01-30 23:58:27 -0800

    illegal shadowing , ..rest

--- NamasteJs/Ep.8/script.js
+++ NamasteJs/Ep.8/script.js
@@ -1,10 +1,25 @@
-function a(){
-    var b = 10;
-    c();
-    function c(){
-        console.log(b);  // 10
-    }
+// function a(){
+//     var b = 10;
+//     c();
+//     function c(){
+//         console.log(b);  // 10
+//     }
 
-}
-a();
-console.log(b);  // reference error ( b is not defined )
+// }
+// a();
+// console.log(b);  // reference error ( b is not defined )
+
+
+
+// var a = 10;
+// {
+//     let a = 1000;
+//     console.log(a);  // worked  (output: 1000)
+// }
+
+// illegal shadowing
+// let b = 10;
+// {
+//     var b = 1000;
+//     console.log(b);   // error SyntaxError : Identifer 'a' has already been declared.
+// }
