77286ea379d4528abadff2e4b0239b350848b7c3
mohdsameerid <mohdsameeredu@gmail.com>
2023-03-27 03:49:00 -0700

    Checking the Sqaure in second array

---
+++ DSA/7.js
@@ -0,0 +1,46 @@
+// Checking Square in another Array
+// input -> Arr1 = [1,2,3,4] , Arr2 = [1,4,9,16]
+// output -> True
+
+// Approach
+// iterate on Arr1
+// make square of every Element in Arr1
+// check that element present in Arr2
+// present { true }
+// not presnt { False}
+
+// using build in function
+function checkSqaure(arr1, arr2) {
+  for (let items of arr1) {
+    let square = items * items;
+    // console.log(square);
+    if (!arr2.includes(square)) return false;
+  }
+  return true;
+}
+const res = checkSqaure([1, 2, 3, 4], [1, 4, 9, 10020]);
+console.log(res);
+// Time Complexity O(n*n)
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// (2) Approach Without using Build in function
+
+function isSquareinAnotherArray(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    let isSqaure = false;
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] * arr1[i] === arr2[j]) {
+        isSqaure = true;
+      }
+      if (j === arr2.length - 1) {
+        if (!isSqaure) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+const res2 = isSquareinAnotherArray([1, 2, 3, 4], [1, 4, 9, 26]);
+console.log(res2);
+// Time Complexity O(n*n)
