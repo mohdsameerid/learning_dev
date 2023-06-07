5896d9244978d4cd87d87b5fc42e715e753fb314
mohdsameerid <mohdsameeredu@gmail.com>
2023-06-07 07:31:21 -0700

    Recursion

--- DSA/8.js
+++ DSA/8.js
@@ -1,2 +1,17 @@
 // Recursion - when function call itself
 //  If the function call itself there must be an end point otherwise it will call infinite time.
+
+
+console.log("Welcome to Programiz!");
+
+let counter = 1;
+function demo(number){
+    if(counter > number){
+        return false;
+    }
+    
+    console.log("Counter ",counter);
+    counter++;
+    demo(number);
+}
+demo(10);
